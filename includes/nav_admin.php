d1067cdb217dbcf3abe587d3aecf772ec2eb1de7
Kelvin Macharia <ngunyimacharia@gmail.com>
2018-10-27 14:09:05 +0000

    Resolved merge conflicts

--- includes/nav_admin.php
+++ includes/nav_admin.php
@@ -1,67 +1,26 @@
 <nav class="admin">
   <ul>
     <li>
-<<<<<<< HEAD
-      <a href="http://localhost/grubbie/user/view.php">
-        <img
-        src="http://localhost/grubbie/assets/images/group_icon_inv.png"
-        title="Group Icon"
-        alt="Group icon" />
-        Users
-=======
+
       <a href="http://localhost/grubbie/user/manage.php">
         <i class="far fa-user" title="Users"></i>
         <span>Users</span>
->>>>>>> 1d85dec7ff8673344b9547df6bdb0d25838c2fae
       </a>
     </li>
     <li>
       <a href="http://localhost/grubbie/rating/rate.php">
-<<<<<<< HEAD
-        <img
-        src="http://localhost/grubbie/assets/images/food_icon_inv.png"
-        title="Food Icon"
-        alt="Food icon" />
-        Meals
-=======
         <i class="fas fa-utensils" title="Meals"></i>
         <span>Meals</span>
->>>>>>> 1d85dec7ff8673344b9547df6bdb0d25838c2fae
       </a>
     </li>
     <li>
       <a href="http://localhost/grubbie/orders/view.php">
-<<<<<<< HEAD
-        <img
-        src="http://localhost/grubbie/assets/images/inventory_icon_inv.png"
-        title="Inventory Icon"
-        alt="Inventory icon" />
-        Inventory
-=======
         <i class="far fa-clipboard"></i>
         <span>Inventory</span>
->>>>>>> 1d85dec7ff8673344b9547df6bdb0d25838c2fae
       </a>
     </li>
     <li>
       <a href="http://localhost/grubbie/orders/view.php">
-<<<<<<< HEAD
-        <img
-        src="http://localhost/grubbie/assets/images/rate_icon_inv.png"
-        title="Rate Icon"
-        alt="Rate icon" />
-        Ratings
-      </a>
-    </li>
-    <li>
-      <a href="http://localhost/grubbie/orders/view.php">
-        <img
-        src="http://localhost/grubbie/assets/images/notifications_icon.png"
-        title="Notifications Icon"
-        alt="Notifications icon" />
-        Messages
-      </a>
-=======
         <i class="far fa-thumbs-up" title="Ratings"></i>
         <span>Ratings</span>
       </a>
@@ -121,7 +80,6 @@
           </div>
         </div>
       </div>
->>>>>>> 1d85dec7ff8673344b9547df6bdb0d25838c2fae
     </li>
   </ul>
 </nav>
