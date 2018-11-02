b262dc2180d59f98d4b439d3cec62c63c0d22fe0
Kelvin Macharia <ngunyimacharia@gmail.com>
2018-11-02 01:06:59 +0000

    Logo link to home

--- includes/header-admin.php
+++ includes/header-admin.php
@@ -1,17 +1,19 @@
 <html>
-  <head>
-    <title>Grubbie</title>
-      <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-    <link rel='stylesheet' href="http://localhost/grubbie/assets/css/main.css" />
-  </head>
-  <body>
-    <div id="body-overlay"></div>
-    <header>
+<head>
+  <title>Grubbie</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <link rel='stylesheet' href="http://localhost/grubbie/assets/css/main.css" />
+</head>
+<body>
+  <div id="body-overlay"></div>
+  <header>
+    <a href = "http://localhost/grubbie">
       <div id="logo">
         <img src="http://localhost/grubbie/assets/images/logo.png" alt="logo" title="logo"/>
         <div id="logo-caption">Gru<span>bb</span>ie</div>
       </div>
-      <?php include("nav_admin.php"); ?>
-    </header>
-    <div id="content-container">
+    </a>
+    <?php include("nav_admin.php"); ?>
+  </header>
+  <div id="content-container">
--- includes/header.php
+++ includes/header.php
@@ -1,17 +1,19 @@
 <html>
-  <head>
-    <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Grubbie</title>
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-    <link rel='stylesheet' href="http://localhost/grubbie/assets/css/main.css" />
-  </head>
-  <body>
-    <div id="body-overlay"></div>
-    <header>
+<head>
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>Grubbie</title>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <link rel='stylesheet' href="http://localhost/grubbie/assets/css/main.css" />
+</head>
+<body>
+  <div id="body-overlay"></div>
+  <header>
+    <a href = "http://localhost/grubbie">
       <div id="logo">
         <img src="http://localhost/grubbie/assets/images/logo.png" alt="logo" title="logo"/>
         <div id="logo-caption">Gru<span>bb</span>ie</div>
       </div>
-      <?php include("nav_user.php"); ?>
-    </header>
-    <div id="content-container">
+    </a>
+    <?php include("nav_user.php"); ?>
+  </header>
+  <div id="content-container">
