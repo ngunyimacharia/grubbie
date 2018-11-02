005793c097ed161894c5189aee2f88343c5e8007
Kelvin Macharia <ngunyimacharia@gmail.com>
2018-11-02 01:00:04 +0000

    Notifications implementation

--- includes/footer-admin.php
+++ includes/footer-admin.php
@@ -3,5 +3,6 @@
 <?php include('nav_admin.php'); ?>
 </footer>
 <div class="clearfix"><br/></div>
+<script src="http://localhost/grubbie/assets/js/main.js" type="text/javascript"></script>
 </body>
 </html>
--- includes/nav_admin.php
+++ includes/nav_admin.php
@@ -31,19 +31,69 @@
       <a href="http://localhost/grubbie/orders/view.php">
         <img
         src="http://localhost/grubbie/assets/images/rate_icon_inv.png"
-        title="Rate Icon"
+        title="Ratings"
         alt="Rate icon" />
         Ratings
       </a>
     </li>
     <li>
-      <a href="http://localhost/grubbie/orders/view.php">
-        <img
-        src="http://localhost/grubbie/assets/images/notifications_icon.png"
-        title="Notifications Icon"
-        alt="Notifications icon" />
+      <a  id="notifications" href="http://localhost/grubbie/user/notifications.php">
+        <div>
+          <img
+          src="http://localhost/grubbie/assets/images/notifications_icon.png"
+          title="Messages"
+          alt="Notifications icon" />
+          <div class="notifications-notice">&nbsp;</div>
+        </div>
         Messages
       </a>
+      <div class="notifications-container">
+        <div class="arrow-up"></div>
+        <div class="notification">
+
+          <img src="http://localhost/grubbie/assets/images/inventory_icon_inv.png" alt="Clipboard" title="Clipboard"/>
+          <div>
+            <a href="http://localhost/grubbie/orders/view.php"><p>The menu for this week has been sent</p></a>
+            <span class="timestamp">1 hour ago</span>
+          </div>
+        </div>
+        <div class="notification">
+
+          <img src="http://localhost/grubbie/assets/images/inventory_icon_inv.png" alt="Clipboard" title="Clipboard"/>
+          <div>
+            <a href="http://localhost/grubbie/orders/view.php">
+              <p>
+                The menu for this week has been sent
+              </p>
+            </a>
+            <span class="timestamp">1 day ago</span>
+          </div>
+        </div>
+        <div class="notification">
+
+          <img src="http://localhost/grubbie/assets/images/inventory_icon_inv.png" alt="Clipboard" title="Clipboard"/>
+          <div>
+            <a href="http://localhost/grubbie/orders/view.php"><p>The menu for this week has been sent</p></a>
+            <span class="timestamp">7 days ago</span>
+          </div>
+        </div>
+        <div class="notification">
+
+          <img src="http://localhost/grubbie/assets/images/inventory_icon_inv.png" alt="Clipboard" title="Clipboard"/>
+          <div>
+            <a href="http://localhost/grubbie/orders/view.php"><p>The menu for this week has been sent</p></a>
+            <span class="timestamp">14 days ago</span>
+          </div>
+        </div>
+        <div class="notification">
+
+          <img src="http://localhost/grubbie/assets/images/inventory_icon_inv.png" alt="Clipboard" title="Clipboard"/>
+          <div>
+            <a href="http://localhost/grubbie/orders/view.php"><p>The menu for this week has been sent</p></a>
+            <span class="timestamp">21 days ago</span>
+          </div>
+        </div>
+      </div>
     </li>
   </ul>
 </nav>
