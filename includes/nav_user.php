047d0a07277faa84327c30f96fad15aa08800104
Kelvin Macharia <ngunyimacharia@gmail.com>
2018-11-02 01:37:35 +0000

    Navigation changes

--- includes/nav_user.php
+++ includes/nav_user.php
@@ -3,7 +3,7 @@
     <li>
       <a href="http://localhost/grubbie/user/view.php">
         <img
-        src="http://localhost/grubbie/assets/images/profile_icon.png"
+        src="http://localhost/grubbie/assets/images/group_icon_inv.png"
         title="Profile"
         alt="Profile icon" />
         Profile
@@ -12,7 +12,7 @@
     <li>
       <a href="http://localhost/grubbie/rating/rate.php">
         <img
-        src="http://localhost/grubbie/assets/images/rate_icon.png"
+        src="http://localhost/grubbie/assets/images/rate_icon_inv.png"
         title="Rate"
         alt="Rate icon" />
         Rate
@@ -21,20 +21,30 @@
     <li>
       <a href="http://localhost/grubbie/orders/view.php">
         <img
-        src="http://localhost/grubbie/assets/images/meal_icon.png"
+        src="http://localhost/grubbie/assets/images/food_icon_inv.png"
         title="Meals"
         alt="Meal icon" />
         Meals
       </a>
     </li>
     <li>
-      <a href="http://localhost/grubbie/orders/view.php">
-        <img
-        src="http://localhost/grubbie/assets/images/notifications_icon.png"
-        title="Messages"
-        alt="Notifications icon" />
+      <a  id="notifications" href="http://localhost/grubbie/user/notifications.php">
+        <div>
+          <img
+          src="http://localhost/grubbie/assets/images/notifications_icon.png"
+          title="Messages"
+          alt="Notifications icon" />
+        </div>
         Messages
       </a>
+      <div class="notifications-container">
+        <div class="arrow-up"></div>
+        <div class="notification">
+          <div>
+            <a href="#"><p>No new notifications</p></a>
+          </div>
+        </div>
+      </div>
     </li>
   </ul>
 </nav>
