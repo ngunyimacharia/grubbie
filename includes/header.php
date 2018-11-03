1d85dec7ff8673344b9547df6bdb0d25838c2fae
Kelvin Macharia <ngunyimacharia@gmail.com>
2018-11-03 09:37:25 +0000

    Gmail style complete

--- includes/header.php
+++ includes/header.php
@@ -4,8 +4,8 @@
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>Grubbie</title>
   <link rel="icon"
-      type="image/png"
-      href="http://localhost/grubbie/assets/images/logo.png">
+  type="image/png"
+  href="http://localhost/grubbie/assets/images/logo.png">
   <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
   <link rel='stylesheet' href="http://localhost/grubbie/assets/css/main.css" />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
