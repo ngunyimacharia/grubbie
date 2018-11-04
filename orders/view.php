7123c4c20d946c8de24fe40eb682767311fff554
Esther mwangi <esthermwangi50@gmail.com>
2018-11-04 21:41:08 +0000

    created orders page

--- orders/view.php
+++ orders/view.php
@@ -1,7 +1,37 @@
 <?php include('../includes/header.php'); ?>
     <h1>View your orders</h1>
     <?php include ('../includes/ui/days.php'); ?>
-    <?php include ('../includes/ui/rounded_div.php'); ?>
-    <?php include ('../includes/ui/signup.php'); ?>
-    <?php include ('../includes/ui/signin.php'); ?>
+    <!-- breakfast lunch dinner -->
+    <div class="bls">
+    	<button class="breakfast">Breakfast</button>
+    	<button class="lunch">Lunch</button>
+    	<button class="dinner">Dinner</button>
+    </div>
+    Options<br/>
+    <!-- input checkbox, checkbox with dropdown -->
+    <div class="order-options">
+    <input type="checkbox" name="jollofRice" id=""><label for="jollofRice">Jollof Rice</label><i class="fas fa-caret-down"></i><br>
+
+    <input type="checkbox" name="Fried Yam" id=""><label for="Fried Yam">Fried Yam</label><i class="fas fa-caret-down"></i><br>
+
+    <input type="checkbox" name="White Rice" id=""><label for="White Rice">White Rice</label><i class="fas fa-caret-down"></i><br>
+
+    <input type="checkbox" name="Potatoe Chips" id=""><label for="Potatoe Chips">Potatoe Chips</label><i class="fas fa-caret-down"></i><br>
+
+    <input type="checkbox" name="Eba" id=""><label for="Eba">Eba</label><i class="fas fa-caret-down"></i><br>
+
+    <input type="checkbox" name="Eba" id=""><label for="Eba">Eba</label><i class="fas fa-caret-down"></i><br>
+
+    <input type="checkbox" name="Plantain" id=""><label for="Plantain">Plantain</label><i class="fas fa-caret-down"></i><br>
+
+    <input type="checkbox" name="Fufu" id=""><label for="Fufu">Fufu</label><i class="fas fa-caret-down"></i><br>
+
+    <input type="checkbox" name="Fufu" id=""><label for="Fufu">Fufu</label><i class="fas fa-caret-down"></i><br>
+
+    <input type="checkbox" name="Banku" id=""><label for="Banku">Banku</label><i class="fas fa-caret-down"></i><br>
+</div>
+    
+
+    <!-- submit button --><br/>
+    <button>Submit</button>
 <?php include('../includes/footer.php'); ?>
