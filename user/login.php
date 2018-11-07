0d15c689e5156c0cf5d3ace72e7788168544a50e
Solomon Igori <igorisolomon@gmail.com>
2018-11-07 20:43:16 +0000

    finished sign up

--- user/login.php
+++ user/login.php
@@ -9,11 +9,11 @@
     <form action="http://localhost/grubbie/user/manage.php" method = "post">
         <input type = "text" name = "Email" placeholder = "Email"/><p>
         <input type = "password" name = "Password" placeholder = "Password"/><p>
-        <p> <a href = "forgotpassword.php"> Forgot Password</a> </p>
+        <p style='text-align: right'> <a href = "forgotpassword.php"> Forgot Password</a> </p>
         <input type = "submit" name = "Sign Up" value = "Sign In"><p>
     </form>
 
-    <p style = 'padding-left: 50px;'> Don't have an account? <a href = "signup.php">Sign Up</a> </p>
+    <p style = 'text-align: center'> Don't have an account? <a href = "signup.php">Sign Up</a> </p>
 </div>
 
 <?php include("../includes/footer-no-nav.php"); ?>
--- user/signup.php
+++ user/signup.php
@@ -1,21 +1,25 @@
 <?php include("../includes/header-no-nav.php"); ?>
 <div id="grubbie-gradient"></div>
 
-<img src="http://localhost/grubbie/assets/images/profile_icon.png" alt = "logo" >
+<div class="paper">
+    <div class = "logo-white" >
+        <img src="http://localhost/grubbie/assets/images/logo.png" alt = "logo">
+    </div>
 
-<form action = "http://localhost/grubbie/menu/view.php" method="POST">
-    <input type = "text" name = "firstname" placeholder = "First Name"/><p>
-    <input type = "text" name = "lastname" placeholder = "Last Name"/><p>
-    <input type = "text" name = "Email" placeholder = "Email"/><p>
-    <select name = "type">
-        <option value = "EIT"> EIT</option>
-        <option value = "MINC"> MINC</option>
-        <option value = "Fellow"> Teaching Fellow </option>
-        <option value = "Staff"> Incubator Fellow </option><p>
-    <input type = "password" name = "Password" placeholder = "Password"/><p>
-    <input type = "submit" name = "Sign Up" value = "Create Account"><p>
-</form>
+    <form action = "http://localhost/grubbie/menu/view.php" method="POST">
+        <input type = "text" name = "firstname" placeholder = "First Name"/><p>
+        <input type = "text" name = "lastname" placeholder = "Last Name"/><p>
+        <input type = "text" name = "Email" placeholder = "Email"/><p>
+        <select name = "type">
+            <option value = "EIT"> EIT</option>
+            <option value = "MINC"> MINC</option>
+            <option value = "Fellow"> Teaching Fellow </option>
+            <option value = "Staff"> Incubator Fellow </option><p>
+        <input type = "password" name = "Password" placeholder = "Password"/><p>
+        <input type = "submit" name = "Sign Up" value = "Create Account"><p>
+    </form>
+    <p style = 'text-align: center'> Have an account? <a href = "login.php">Sign in</a> </p>
+</div>
 
-<p align = "center"> Have an account? <a href = "login.php">Sign in</a> </p>
 
 <?php include("../includes/footer-no-nav.php"); ?>
