27cb1b53f59cf741191ba8b341cc59dc7b1ef5cc
Solomon Igori <igorisolomon@gmail.com>
2018-11-07 23:02:06 +0000

    profile page

--- user/view.php
+++ user/view.php
@@ -2,17 +2,35 @@
 
 <div class = 'paper'>
 <form action = "view_edit.php">
-  <div id="Translation">
-    <h3>User Name</h3>
-    <p>Department: $dept</p>
-    <p>Country: $count</p>
-    <p>Allergy: $alle</p>
-    <p>Preferred Diet: $vege</p>
+  <div id="Translation" class="cont">
+  <div class="profile">
+    <i class="far fa-user-circle fa-7x"></i>
+    <h3>Kayode</h3>
+  </div>
+    
+    <table>
+      <tr>
+        <td>Department:</td>
+        <td>EIT</td>
+      </tr>
+      <tr>
+        <td>Country:</td>
+        <td>Nigeria</td>
+      </tr>
+      <tr>
+        <td>Allergy:</td>
+        <td>Warm milk</td>
+      </tr>
+      <tr>
+        <td>Preferred diet:</td>
+        <td>Vegie</td>
+      </tr>
+    </table>
 
     <button class="round-block" type = "submit"> Edit </button><p>
 
     </div>
-    <a href="http://localhost/grubbie" class="round-btn-danger" >Logout</a>
+    <a href="http://localhost/grubbie" class="round-btn" style="float: right">Logout</a>
     </div>
   </form>
   <?php include('../includes/footer.php'); ?>
--- user/view_edit.php
+++ user/view_edit.php
@@ -1,39 +1,61 @@
 <?php include('../includes/header.php'); ?>
 
+<div class="paper">
 <form action = "http://localhost/grubbie/menu/view.php" method="POST">
-<div id="Translation">
-    <h3>User Name</h3>
-    <p>Department: <select name = "dept">
-            <option value = "EIT"> EIT </option>
-            <option value = "MINC"> MINC </option>
-            <option value = "Fellow"> Teaching Fellow </option>
-            <option value = "Staff"> Incubator Fellow </option><p>
-    </select>
+<div id="Translation" class="cont">
+<div class="profile">
+    <i class="far fa-user-circle fa-7x"></i>
+    <h3>Kayode</h3>
+  </div>
+    <table>
+    <tr>
+        <td>Department:</td>
+        <td>
+                <select name = "dept">
+                        <option value = "EIT"> EIT </option>
+                        <option value = "MINC"> MINC </option>
+                        <option value = "Fellow"> Teaching Fellow </option>
+                        <option value = "Staff"> Incubator Fellow </option>
+                </select>
+        </td>
+    </tr>
+    <tr>
+        <td>Country:</td>
+        <td>
+                <select name = "count">
+                        <option value = "Kenya"> Kenya </option>
+                        <option value = "Ghana"> Ghana </option>
+                        <option value = "Sudan"> Sudan </option>
+                        <option value = "South_Africa"> South Africa </option>
+                        <option value = "Nigeria"> Nigeria </option>
+                        <option value = "Zimbabwe"> Zimbabwe </option>
+                        <option value = "Cameroon"> Cameroon </option>
+                        <option value = "Mali"> Mali </option>
+                        <option value = "Ivory_Coast "> Ivory Coast </option>
+                </select>
+        </td>
+    </tr>
+    <tr>
+        <td>Allergy:</td>
+        <td>allergy</td>
+    </tr>
+    <tr>
+        <td>Preferred Diet:</td>
+        <td>
+                <select name = "allerg">
+                        <option value = "Vege"> Vegetarian </option>
+                        <option value = "Omni"> Omnivorous </option>
+                        <option value = "Pesc"> Pescaterian </option>
+                        <option value = "Vegan"> Vegan </option>
+                </select>
+        </td>
+    </tr>
+    </table>
 
-    <p>Country: <select name = "count">
-            <option value = "Kenya"> Kenya </option>
-            <option value = "Ghana"> Ghana </option>
-            <option value = "Sudan"> Sudan </option>
-            <option value = "South_Africa"> South Africa </option>
-            <option value = "Nigeria"> Nigeria </option>
-            <option value = "Zimbabwe"> Zimbabwe </option>
-            <option value = "Cameroon"> Cameroon </option>
-            <option value = "Mali"> Mali </option>
-            <option value = "Ivory_Coast "> Ivory Coast </option>
-    </select>
+    <button class="round-block" type = "submit"> Save </button>
 
-    <p> Allergy: $aller</p>
-
-    <p>Preferred Diet: <select name = "allerg">
-            <option value = "Vege"> Vegetarian </option>
-            <option value = "Omni"> Omnivorous </option>
-            <option value = "Pesc"> Pescaterian </option>
-            <option value = "Vegan"> Vegan </option></p>
-    </select>
-
-
-    <button class="round-block" type = "submit"> Save </button><p>
-
+</div>
+</form>
 </div>
 
 <?php include('../includes/footer.php'); ?>
