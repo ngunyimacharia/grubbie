7cacac57600523aa100d29eb28a3c08b352dd1a0
Solomon Igori <igorisolomon@gmail.com>
2018-11-07 20:19:01 +0000

    worked on the signup page

--- user/login.php
+++ user/login.php
@@ -1,15 +1,19 @@
 <?php include("../includes/header-no-nav.php"); ?>
 <div id="grubbie-gradient"></div>
 
-<img src="http://localhost/grubbie/assets/images/profile_icon.png" alt = "logo" >
+<div class="paper">
+    <div class = "logo-white" >
+        <img src="http://localhost/grubbie/assets/images/logo.png" alt = "logo">
+    </div>
 
-<form action="http://localhost/grubbie/user/manage.php" method = "post">
-    <input type = "text" name = "Email" placeholder = "Email"/><p>
-    <input type = "password" name = "Password" placeholder = "Password"/><p>
-    <p align = "right"> <a href = "forgotpassword.php"> Forgot Password</a> </p>
-    <input type = "submit" name = "Sign Up" value = "Sign In"><p>
-</form>
+    <form action="http://localhost/grubbie/user/manage.php" method = "post">
+        <input type = "text" name = "Email" placeholder = "Email"/><p>
+        <input type = "password" name = "Password" placeholder = "Password"/><p>
+        <p> <a href = "forgotpassword.php"> Forgot Password</a> </p>
+        <input type = "submit" name = "Sign Up" value = "Sign In"><p>
+    </form>
 
-<p align = "center"> Don't have an account? <a href = "signup.php">Sign Up</a> </p>
+    <p style = 'padding-left: 50px;'> Don't have an account? <a href = "signup.php">Sign Up</a> </p>
+</div>
 
 <?php include("../includes/footer-no-nav.php"); ?>
