0cad08c8f7a5c00b7be1408d34de2f689d693a80
Solomon Igori <igorisolomon@gmail.com>
2018-11-07 19:04:33 +0000

    paper-like form

--- user/splash.php
+++ user/splash.php
@@ -1,14 +1,14 @@
 <?php include("../includes/header-no-nav.php"); ?>
 <div id="grubbie-gradient"></div>
+<div class='paper'>
+    <div class = "logo-white" >
+        <img src="http://localhost/grubbie/assets/images/profile_icon.png" alt = "logo">
+    </div>
 
-<div class = "logo-white" >
-    <img src="http://localhost/grubbie/assets/images/profile_icon.png" alt = "logo">
+    <form>
+        <button class="round-block" type = "submit" formaction = "signup.php"> Create Account</button><p>
+        
+        <button class="round-block" type = "submit" formaction = "login.php"> Sign In </button><p>
+    </form>
 </div>
-
-<form>
-<button class="round-block" type = "submit" formaction = "signup.php"> Create Account</button><p>
-   
-<button class="round-block" type = "submit" formaction = "login.php"> Sign In </button><p>
-</form>
-
 <?php include("../includes/footer-no-nav.php"); ?>
