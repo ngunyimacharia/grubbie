357e51d38da5acb7a3172c085093ee16b7b3b8ae
Solomon Igori <igorisolomon@gmail.com>
2018-11-08 13:24:53 +0000

    fix the combined page

--- rating/rate.php
+++ rating/rate.php
@@ -9,7 +9,7 @@
             <div class="rounded-image"></div>
             <div class="rate-text">
                 Tea and Bread <br/>
-                <?php include('../includes/ui/stars.php'); ?>  <br/>
+                <?php include('../includes/ui/stars.php'); ?>
                 <input type="text" name="" id="">
             </div>
         </div>
@@ -20,18 +20,18 @@
             <div class="rounded-image"></div>
             <div class="rate-text">
                 Jollof Rice <br/>
-                <?php include('../includes/ui/stars.php'); ?> <br/>
+                <?php include('../includes/ui/stars.php'); ?>
                 <input type="text" name="" id="">
             </div>
         </div>
     </div>
-
+    
     <div class="rounded-container">
         <div class="rate-wrapper">
             <div class="rounded-image"></div>
             <div class="rate-text">
                 Rice and Stew <br/>
-                <?php include('../includes/ui/stars.php'); ?>  <br/>
+                <?php include('../includes/ui/stars.php'); ?>
                 <input type="text" name="" id="">
             </div>
         </div>
---
+++ user/meals.php
@@ -0,0 +1,25 @@
+<?php include('../includes/header.php'); ?>
+<h1>View your menu</h1>
+<!-- days of the week -->
+<?php include('../includes/ui/days.php'); ?>
+
+<div class="rounded-container">
+    <div class="rounded-wrapper">
+        <div class="rounded-image"></div>
+        <div class="rounded-text">Tea and Bread</div>
+    </div>
+</div>
+<div class="rounded-container">
+    <div class="rounded-wrapper">
+        <div class="rounded-image"></div>
+        <div class="rounded-text">Jollof rice</div>
+    </div>
+</div>
+<div class="rounded-container">
+    <div class="rounded-wrapper">
+        <div class="rounded-image"></div>
+        <div class="rounded-text">Chicken and chips</div>
+    </div>
+</div>
+
+<?php include('../includes/footer.php'); ?>
