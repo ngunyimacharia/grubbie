d18995b23f2282511024773aa98f1ad3c85f4334
Kelvin Macharia <ngunyimacharia@gmail.com>
2018-11-08 13:37:14 +0000

    Meal view page complete

---
+++ meal/view.php
@@ -0,0 +1,84 @@
+<?php include('../includes/header.php'); ?>
+
+<div class="meal-view">
+  <div class="row">
+      <div class="col-1"></div>
+      <button class="btn-circ col-lg-3 col-md-4 col-sm-3 col-10">
+        <i class="fas fa-caret-left"></i>
+        &nbsp;
+        Back to Menu
+      </button>
+      <div class="col-lg-5 col-md-3 col-sm-4 col-5"></div>
+      <button class="btn-circ col-lg-2 col-md-3 col-sm-3 col-10 pull-right lg">
+        <i class="fas fa-user-check"></i>
+        &nbsp;
+        Refer
+      </button>
+  </div>
+  <div class="clearfix"><br/></div>
+  <div class="clearfix"><br/></div>
+  <div id="Translation" class="row cont">
+    <div class="col-lg-5 col-12">
+      <br/><br/>
+      <div class="icon-container">
+        <div class="icon">
+          <img src="http://localhost/grubbie/assets/images/food.jpg" />
+        </div>
+      </div>
+      <h3 class="text-center">Ewedu + Gbegiri + Amala + Fish</h3>
+      <!-- <div class="row lg">
+        <div class="col-2"></div>
+        <a class="btn-warning col-3" href="http://localhost/grubbie/user/view_edit.php"> Edit </a>
+        <div class="col-1"></div>
+        <a class="btn-danger col-3"  href="http://localhost/grubbie"> Logout </a>
+      </div> -->
+    </div>
+    <div class="col-lg-7 col-12">
+      <div class="data">
+      <div class="row">
+        <div class="col-7"></div>
+        <button class="btn-circ col-sm-3 col-4 xs text-right">
+          <i class="fas fa-user-check"></i>
+          &nbsp;
+          Refer
+        </button>
+      </div>
+        <div>
+          <h3>Description</h3>
+          <p>
+            Amala is the soft, thick grey food in the picture. It’s eaten with stews, naturally with a specific vegetable soup, Ewedu soup (from iron rich mallow leaves). My gran will be eating it with Ewedu soup and a fish tomato and pepper based stew. To me, it’s kind of tasteless with a slightly bitter flavour. But amala lovers will disagree! What I do appreciate about it is that it just glides down your throat. MMMmmm.
+          </p>
+          <p>
+            It’s made from a white powder which comes from dried and pulverised ‘okorondo’ (mini yams). My greatgrandma insisted on the powder from mini yams, because although the powder can come from fully grown yams too, the mini yams have a distinct flavour.
+          </p>
+        </div>
+        <br/>
+        <div>
+          <h3>Ingredients</h3>
+          <p>
+            <ul>
+              <li>300g or 4-6 cups Yam Flour(Elubo)</li>
+              <li>100g Beans</li>
+              <li>400g Ewedu vegetable</li>
+              <li>8 0r more Red pepper</li>
+              <li>10 Small Red Chilli Peppers</li>
+              <li>7 or more Fresh Tomatoes</li>
+              <li>1 bottle of palm oil</li>
+              <li>20g Locust beans</li>
+              <li>¼ oz Potassium salt</li>
+              <li>450g Beef/Fish</li>
+              <li>200g Snails</li>
+              <li>50g Crayfish</li>
+              <li>Maggi/Salt</li>
+              <li>Other seasonings</li>
+            </ul>
+          </p>
+        </div>
+      </div>
+      <div class="clearfix"><br/></div>
+    </div>
+    <div class="clearfix"><br/></div>
+    <div class="clearfix"><br/></div>
+  </div>
+</div>
+<?php include('../includes/footer.php'); ?>
